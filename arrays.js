850924df78168af1027a38647e77ef9e20a7e285
Ioan Scurtu <ioan.scurtu120@gmail.com>
2019-09-04 10:48:23 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -17,35 +17,3 @@ function addElementToEndOfArray(addEndConserve, element){
   return [...addEndConserve,"foo"]
 }
 
-var addEndDestruct = [1];
-function destructivelyAddElementToEndOfArray(addEndDestruct, index){
-  addEndDestruct.push("foo");
-  return addEndDestruct;
-}
-
-var array = [1,2,3];
-function accessElementInArray(array,index){
-  return array[2];
-}
-
-var removeBeginningDestruct = [1,2,3];
-function destructivelyRemoveElementFromBeginningOfArray(removeBeginningDestruct){
-  removeBeginningDestruct.shift();
-  return removeBeginningDestruct;
-}
-
-var removeBeginningConserve = [1,2,3];
-function removeElementFromBeginningOfArray(removeBeginningConserve){
-  return removeBeginningConserve.slice(1);
-}
-
-var removeEndDestruct = [1,2,3];
-function destructivelyRemoveElementFromEndOfArray(removeEndDestruct){
-  removeEndDestruct.pop();
-  return removeEndDestruct;
-}
-
-var removeEndConserve = [1,2,3];
-function removeElementFromEndOfArray(removeEndConserve){
-  return removeEndConserve.slice(0, removeEndConserve.length - 1);
-}
