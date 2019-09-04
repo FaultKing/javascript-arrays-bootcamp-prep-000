247d378990f04222ef9842031e6fdcb4c0830295
Ioan Scurtu <ioan.scurtu120@gmail.com>
2019-09-04 10:49:05 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,18 +1,33 @@
-ar chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
-var addBeginningConserve = [1];
-
-function addElementToBeginningOfArray(addBeginningConserve, element){
-  return ["foo",...addBeginningConserve]
+var addEndDestruct = [1];
+function destructivelyAddElementToEndOfArray(addEndDestruct, index){
+  addEndDestruct.push("foo");
+  return addEndDestruct;
 }
 
-var addBeginningDestruct = [1];
-function destructivelyAddElementToBeginningOfArray(addBeginningDestruct, element){
-  addBeginningDestruct.unshift("foo");
-  return addBeginningDestruct;
+var array = [1,2,3];
+function accessElementInArray(array,index){
+  return array[2];
 }
 
-var addEndConserve = [1];
-function addElementToEndOfArray(addEndConserve, element){
-  return [...addEndConserve,"foo"]
+var removeBeginningDestruct = [1,2,3];
+function destructivelyRemoveElementFromBeginningOfArray(removeBeginningDestruct){
+  removeBeginningDestruct.shift();
+  return removeBeginningDestruct;
+}
+
+var removeBeginningConserve = [1,2,3];
+function removeElementFromBeginningOfArray(removeBeginningConserve){
+  return removeBeginningConserve.slice(1);
+}
+
+var removeEndDestruct = [1,2,3];
+function destructivelyRemoveElementFromEndOfArray(removeEndDestruct){
+  removeEndDestruct.pop();
+  return removeEndDestruct;
+}
+
+var removeEndConserve = [1,2,3];
+function removeElementFromEndOfArray(removeEndConserve){
+  return removeEndConserve.slice(0, removeEndConserve.length - 1);
 }
