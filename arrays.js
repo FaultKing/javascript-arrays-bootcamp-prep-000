7267064b556896f51805aee92a4f5df3ec2c6426
Ioan Scurtu <ioan.scurtu120@gmail.com>
2019-09-04 10:51:43 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,3 +1,21 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+var addBeginningConserve = [1];
+
+function addElementToBeginningOfArray(addBeginningConserve, element){
+  return ["foo",...addBeginningConserve]
+}
+
+var addBeginningDestruct = [1];
+function destructivelyAddElementToBeginningOfArray(addBeginningDestruct, element){
+  addBeginningDestruct.unshift("foo");
+  return addBeginningDestruct;
+}
+
+var addEndConserve = [1];
+function addElementToEndOfArray(addEndConserve, element){
+  return [...addEndConserve,"foo"]
+}
 
 var addEndDestruct = [1];
 function destructivelyAddElementToEndOfArray(addEndDestruct, index){
